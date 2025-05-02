79b4cbce6b3571cc75d369b23e6bce0b4e4f803b
Muriz12 <144159753+Muriz12@users.noreply.github.com>
2025-05-02 10:56:32 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/* style.css */
+
+/* (O CSS que você criou continua igual) */
+
+/* Agora o scripts.js */
+
+// scripts.js
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('showing');
+}
